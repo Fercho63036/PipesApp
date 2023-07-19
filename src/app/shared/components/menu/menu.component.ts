7f2523c6036d65038951a6b6a4bd0ec103767cc9
ArielParicagua <ArielFParicaguaV@gmail.com>
2023-07-19 12:44:57 -0400

    Creacion de la seccion de Pipes personalizados

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -39,8 +39,9 @@ export class MenuComponent implements OnInit{
             icon: 'pi pi-cog',
             items:[
               {
-                label: 'Otro elemento',
+                label: 'Custom pipe',
                 icon: 'pi pi-cog',
+                routerLink:'custom',
               },
             ]
           },
